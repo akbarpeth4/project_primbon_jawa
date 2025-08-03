e79e9aa0d5d3056a5ec051145e787f96d8dda488
Nama Kamu <emailkamu@example.com>
2025-08-03 22:51:26 +0700

    Push all files for initial commit with workflow

---
+++ primbon_jawa_app/android/app/src/main/kotlin/com/example/primbon_jawa_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.primbon_jawa_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ primbon_jawa_app/lib/interface/i_hari_pasaran_calculator.dart
@@ -0,0 +1,5 @@
+import '../services/hari_pasaran_result.dart';
+
+abstract class IHariPasaranCalculator {
+  HariPasaranResult hitung(int jumlahHari);
+}
