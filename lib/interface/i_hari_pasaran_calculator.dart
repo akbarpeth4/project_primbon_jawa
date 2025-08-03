a6ba40dadf1274899361935a771f704ac1e5ea27
akbarpeth4 <sahidakbar013@gmail.com>
2025-08-03 20:29:12 +0700

    Delete lib directory

--- lib/interface/i_hari_pasaran_calculator.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_application_1/services/hari_pasaran_result.dart';
-
-abstract class IHariPasaranCalculator {
-  HariPasaranResult hitung(int jumlahHari);
-}
--- lib/services/hari_pasaran_result.dart
+++
@@ -1,6 +0,0 @@
-class HariPasaranResult {
-  final String hari;
-  final String pasaran;
-
-  HariPasaranResult(this.hari, this.pasaran);
-}
